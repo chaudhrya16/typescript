60c59f08c16c26d9df20fcf394ededf37edb89a5
chaudhrya16 <chaudhry_a@yahoo.com>
2022-12-23 21:43:33 -0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -16,8 +16,31 @@ class LinkedListNode {
         }
         
     }
-}
 
-let node1 = new LinkedListNode(5, undefined);
-let node2 = new LinkedListNode(6, node1);
+    copy () : LinkedListNode {
+        return new LinkedListNode(this.data, undefined);
+    }
+
+    reversed () : LinkedListNode | undefined  {
+        if (this.next === undefined) {
+            return this;
+        }
+        else {
+            let thisNodeCopy : LinkedListNode =  this.copy();
+            let thisNode: LinkedListNode = this;
+            while (thisNode.next !== undefined) {
+                let next = thisNode.next;
+                let nextNodeCopy = next.copy();
+                nextNodeCopy.next = thisNodeCopy;
+                thisNodeCopy = nextNodeCopy;
+                thisNode = next;
+            }
+            return thisNodeCopy;
+        }
+    }
+}
+let node0 = new LinkedListNode(6, undefined);
+let node1 = new LinkedListNode(5, node0);
+let node2 = new LinkedListNode(4, node1);
 node2.print();
+node2.reversed()?.print();
